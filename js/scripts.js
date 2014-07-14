6a535de87edfc705fba1339f90338f80487b573e
JJ Idt <johnidt@gmail.com>
2014-07-13 22:27:36 -0700

    program splits words to array and checks for doubles

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-var repeatRemover = function(sentence) {
-	return "life is a highway.";
+var repeatRemover = function (sentence) {
+	var split_words = sentence.split(" ");
+	for (var i = 0; i < split_words.length; i++) {
+		if (split_words[i] === split_words[i+1]) {
+			split_words.splice(i,1);
+		}
+	}
+	return split_words.join(" ");
 };
--- specs/specs.js
+++ specs/specs.js
@@ -6,7 +6,7 @@ describe("repeatRemover", function(){
 		repeatRemover("I i want to ride it all night long").should.equal("I want to ride it all night long");
 
 	});
-	it("takes a sentence that hsas the last word of the sentence repeated and returns the sentence with the repeat removed", function(){
+	it("takes a sentence that has the last word of the sentence repeated and returns the sentence with the repeat removed", function(){
 		repeatRemover("If you're going my way way.").should.equal("If you're going my way");
 	});
 	it("takes a sentence that has multiple repeated words and removes the repeats.", function(){
